70edfb8f56d35850ed76b5a827507dba38dc1005
alaminislamrahat <alaminislamrahat@gmail.com>
2025-09-06 01:03:37 +0600

    implement a 3d model on hero section

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -2,6 +2,10 @@ import { useRef } from "react"
 import AnimatedLines from "../components/AnimatedLines"
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { Canvas } from "@react-three/fiber";
+import { Planet } from "../components/Planet";
+import { AmbientLight } from "three";
+import { Environment, Float, Lightformer } from "@react-three/drei";
 
 const Hero = () => {
     const contextRef = useRef(null);
@@ -42,6 +46,25 @@ results driven webs`;
                     </div>
                 </div>
             </div>
+            <figure
+            style={{height: "100vh", width : "100vh"}}
+            className="absolute left-[50vh] -z-50 inset-0">
+                <Canvas shadows camera={{position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}>
+                    <ambientLight intensity={0.5}/>
+                    <Float speed={0.5}>
+                         <Planet/>
+                    </Float>
+                   
+                    <Environment resolution={256}>
+                        <group rotation={[-Math.PI/3, 4, 1]}>
+                            <Lightformer form={"circle"} intensity={2} position={[0, 5, -9]} scale={10}/>
+                            <Lightformer form={"circle"} intensity={2} position={[0, 3, 1]} scale={10}/>
+                            <Lightformer form={"circle"} intensity={2} position={[-5, -1, -1]} scale={10}/>
+                            <Lightformer form={"circle"} intensity={2} position={[10, 1, 0]} scale={16}/>
+                        </group>
+                    </Environment>
+                </Canvas>
+            </figure>
         </section>
     )
 }
