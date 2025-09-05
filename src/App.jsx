663283a3352646feb6e52c639d6611b24d453c6f
alaminislamrahat <alaminislamrahat@gmail.com>
2025-09-05 17:15:32 +0600

    working on my porfolio project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './section/Navbar'
+
+const App = () => {
+  return (
+    <div className='relative min-h-screen w-screen overflow-x-auto'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
