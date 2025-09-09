6bbadff965a9f10b8a72964e789eca0fe96e17d1
alaminislamrahat <alaminislamrahat@gmail.com>
2025-09-09 20:58:00 +0600

    initial design done

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,46 @@ import About from './section/About'
 import Works from './section/Works'
 import ContactSummery from './section/ContactSummery'
 import Contact from './section/Contact'
+import { useProgress } from '@react-three/drei'
+import { useEffect, useState } from 'react'
 
 gsap.registerPlugin(ScrollTrigger)
 const App = () => {
 
+  const { progress } = useProgress();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (progress === 100) {
+      setIsReady(true)
+    }
+  }, [progress])
+
   return (
-    <ReactLenis root className='relative min-h-screen w-screen '>
-      <Navbar />
-       <Hero />
-      <ServiceSummery />
-      <Services />
-      <About />
-      <Works />
-      <ContactSummery/>
-      <Contact/>
+    <ReactLenis root className='relative min-h-screen w-screen overflow-x-hidden'>
+      {!isReady && (
+        <div className='fixed inset-0 z-[999] flex flex-col items-center justify-center bg-black text-white transition-opacity duration-700 font-light '>
+          <p className='mb-4 text-xl tracking-widest animate-pulse'>
+            {Math.floor(progress)}%
+          </p>
+          <div className='relative h-1 overflow-hidden rounded w-60 bg-white/20'>
+          <div
+          style={{width : `${progress}%`}}
+           className='absolute inset-0 top-0 left-0 h-full transition-all duration-300 bg-white'></div>
+          </div>
+        </div>
+      )}
+
+      <div className={`${progress === 100 ? "opacity-100" : "opacity-0"} transition-opacity duration-1000`}>
+        <Navbar />
+        <Hero />
+        <ServiceSummery />
+        <Services />
+        <About />
+        <Works />
+        <ContactSummery />
+        <Contact />
+      </div>
     </ReactLenis>
   )
 }
