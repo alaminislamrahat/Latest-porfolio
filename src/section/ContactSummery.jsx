b3841387a1b82838d5be09aaf436f3b9326be1fc
alaminislamrahat <alaminislamrahat@gmail.com>
2025-09-09 13:32:54 +0600

    done working on contact summery section

--- src/section/ContactSummery.jsx
+++ src/section/ContactSummery.jsx
@@ -1,11 +1,46 @@
 import { useRef } from "react"
+import Marquee from "../components/Marquee"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const ContactSummery = () => {
-  const containerRef = useRef(null)
+  const containerRef = useRef(null);
+  const items = ["Innovation", "Precision", "Trust", "Collaboration", "Exellece"];
+  const items2 = ["contact Us", "contact Us", "contact Us", "contact Us", "contact Us", "contact Us",];
+
+  useGSAP(()=>{
+    gsap.to(containerRef.current,{
+      scrollTrigger:{
+        trigger: containerRef.current,
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true
+      }
+    })
+  },[])
+
   return (
-    <section 
-    ref={containerRef} 
-    className="flex flex-col justify-between items-center min-h-screen gap-12 mt-16"></section>
+    <section
+      ref={containerRef}
+      className="flex flex-col justify-between items-center min-h-screen gap-12 mt-16">
+      <Marquee items={items} />
+      <div className="overflow-hidden font-light text-center contact-text-responsive">
+        <p>" Let's build a <br />
+          <span className="font-normal">memorable</span> & {" "} <span className="italic">inspiring</span> <br />
+          web application <span className="text-gold">together</span> "
+        </p>
+      </div>
+
+      <Marquee
+        reverse={true}
+        className="bg-transparent text-black border-y-2"
+        iconClassName="stroke-gold stroke-2 text-primary"
+        icon="material-symbols-light:square"
+        items={items2} />
+
+    </section>
   )
 }
 
