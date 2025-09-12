5f4dbfb89535314ecb4c683f416e9fa5e6213192
alaminislamrahat <alaminislamrahat@gmail.com>
2025-09-12 21:29:37 +0600

    afl

--- src/App.jsx
+++ src/App.jsx
@@ -16,29 +16,29 @@ import { useEffect, useState } from 'react'
 gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
-  const { progress } = useProgress()
-  const [isReady, setIsReady] = useState(false)
+  // const { progress } = useProgress()
+  // const [isReady, setIsReady] = useState(false)
 
-  useEffect(() => {
-    if (progress === 100) {
-      const handlePageLoad = () => {
-        // add small delay for smooth fade
-        setTimeout(() => setIsReady(true), 500)
-      }
+  // useEffect(() => {
+  //   if (progress === 100) {
+  //     const handlePageLoad = () => {
+  //       // add small delay for smooth fade
+  //       setTimeout(() => setIsReady(true), 500)
+  //     }
 
-      if (document.readyState === 'complete') {
-        handlePageLoad()
-      } else {
-        window.addEventListener('load', handlePageLoad)
-        return () => window.removeEventListener('load', handlePageLoad)
-      }
-    }
-  }, [progress])
+  //     if (document.readyState === 'complete') {
+  //       handlePageLoad()
+  //     } else {
+  //       window.addEventListener('load', handlePageLoad)
+  //       return () => window.removeEventListener('load', handlePageLoad)
+  //     }
+  //   }
+  // }, [progress])
 
   return (
     <ReactLenis root className='relative min-h-screen w-screen overflow-x-hidden'>
       {/* Loader */}
-      <div
+      {/* <div
         className={`fixed inset-0 z-[999] flex flex-col items-center justify-center bg-black text-white font-light transition-opacity duration-700
         ${isReady ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}
       >
@@ -51,13 +51,13 @@ const App = () => {
             className='absolute inset-0 top-0 left-0 h-full transition-all duration-300 bg-white'
           ></div>
         </div>
-      </div>
+      </div> */}
 
       {/* Main content */}
       <div
-        className={`${
-          isReady ? 'opacity-100' : 'opacity-0'
-        } transition-opacity duration-1000`}
+        // className={`${
+        //   isReady ? 'opacity-100' : 'opacity-0'
+        // } transition-opacity duration-1000`}
       >
         <Navbar />
         <Hero />
